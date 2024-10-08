ed527b0443e975eba936a2b5f242186fa5d58c6d
vege4944 <vege4944@gmail.com>
2024-10-09 00:13:33 +0900

    level 2-7 N+1
    The FETCH was already added.

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u " +
+    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u " + // level 2-7 FETCH 이미 전에 추가했었습니다!
             "WHERE (:weather IS NULL OR t.weather = :weather)" +
             "AND (:startDate IS NULL OR t.modifiedAt >= :startDate ) " +
             "AND (:endDate IS NULL OR t.modifiedAt <= :endDate)" +
@@ -22,7 +22,7 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
                                                  @Param("startDate") Date startDate,
                                                  @Param("endDate") Date endDate,
                                                  Pageable pageable);
-
+//신민아 튜터님께서 메소드명 간결하게 쓰는거 추천해주심! ex) getTodoByCondition / native query > jpql > query test
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +
             "WHERE t.id = :todoId")
